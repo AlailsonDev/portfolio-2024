d4b7cb88140f53487bf685a118a81f055a419aa0
Alailson Barbosa <alailsonbcsilva@gmail.com>
2024-05-28 13:27:52 -0300

    ajustes tecnicos

---
+++ portfolio-2024/src/Contato/Contato.jsx
@@ -0,0 +1,74 @@
+import '../Contato/Contato.css';
+import { useState } from 'react';
+import telefone from '../assets/telefone.png';
+import whatsapp from '../assets/whatsapp.png';
+import emailImg from '../assets/e-mail.png';
+import emailjs from '@emailjs/browser';
+
+const Contato = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    function sendEmail(e) {
+        e.preventDefault();
+        
+        if(name === '' || email === '' || message === "") {
+            alert('Preencha todos os campos')
+            return;
+        }
+
+        const templateParams = {
+            from_name: name,
+            message: message,
+            email: email
+        }
+
+        emailjs.send("service_ms8xqve", "template_7ty2jha", templateParams, "FvnS6irdt91GY0plp")
+        .then((response) => {
+            console.log('Email Enviado!', response.status, response.text)
+            alert('Mensagem Enviada!')
+            setName('')
+            setEmail('')
+            setMessage('')
+        }, (err) => {
+            console.log("ERRO: ", err)
+        })
+    }
+
+    return (
+        <div className="contato">
+            <h2 id='contatos' className="contato-title">Contatos</h2>
+            
+            <form className='form' onSubmit={sendEmail}>
+                <aside className='contato-aside' id="contato-left">
+                    <input className="input-form" type="text" id="name" placeholder="Seu Nome" onChange={(e) => setName(e.target.value)} value={name}/>
+                    <input className="input-form" type="email" id="email" placeholder="Seu E-mail" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    <textarea className="textarea-form" name="" id="message" placeholder="Digite sua mensagem aqui" onChange={(e) => setMessage(e.target.value)} value={message}></textarea>
+                    <input className='submit-btn' type="submit" id="submit"/>
+                </aside>
+                <aside className='contato-aside' id="contato-right">
+                    <div className='div-contato'>
+                        <img className='contato-img' src={telefone} alt="Telefone" />
+                        <h3 className='contato-subtitle'>Telefone</h3>
+                        <p className='contato-paragraph'>(81) 98717-6814</p>
+                    </div>
+                    <div className='div-contato'>
+                        <img className='contato-img' src={whatsapp} alt="Whatsapp" />
+                        <h3 className='contato-subtitle'>Whatsapp</h3>
+                        <p className='contato-paragraph'>(81) 98717-6814</p>
+                    </div>
+                    <div className='div-contato'>
+                        <img className='contato-img' src={emailImg} alt="E-mail" />
+                        <h3 className='contato-subtitle'>E-mail</h3>
+                        <p className='contato-paragraph'>alailsonbcsilva@gmail.com</p>
+                    </div>
+                </aside>
+            </form>
+            
+        </div>
+    )
+} 
+
+export default Contato;
---
+++ portfolio-2024/src/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import '../Footer/Footer.css';
+
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p className='footer-paragraph'>
+            &copy; 2024 Alailson Barbosa. Todos os direitos reservados.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
